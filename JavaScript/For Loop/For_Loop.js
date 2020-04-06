3a7f8a4f0b5d00ca9ee29234d76403cdf3372e76
jeffft9 <diasjefferson2@gmail.com>
2020-04-07 01:24:29 +0530

    JS code Demonstrating usage oof "For" Loop

---
+++ JavaScript/For Loop/For_Loop.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < 5; i++) {
+  console.log("Number is : "+i);
+}
+
+var word = "ABCDEFGHIJK"
+
+for (var i = 0; i < word.length; i++) {
+  console.log(word[i]);word[i]
+}
+
+var word2 = "ABABABABAB"
+for (var i = 0; i < word2.length; i+=2) {
+  console.log(word2[i]);
+}
